d51b12d13978c61a85b83b0d91b5ff898b0554da
Andres Carter <carter.marino@gmail.com>
2015-10-06 16:36:30 -0400

    Seed data was added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,30 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+locations = [
+	[ 'Atlanta','Georgia' ],
+	[ 'Austin', 'Texas' ],
+	[ 'Boston', 'Massachussettes' ],
+	[ 'Chicago', 'Illinois' ],
+	[ 'Hong Kong', 'China' ],
+	[ 'London', 'United Kindom' ],
+	[ 'Los Angeles', 'California' ],
+	[ 'Melbourne', 'Australia' ],
+	[ 'New York', 'New York' ],
+	[ 'San Francisco', 'California' ],
+	[ 'Seattle', 'Washington' ],
+	[ 'Singapore', 'Singapore' ],
+	[ 'Sydney', 'Australia' ],
+	[ 'Washington', 'D.C.' ]
+	]
+
+locations.each do |city, state|
+	Location.create( city: city, state: state )
+end
+
+skills = [ "Javascript", "React.js", "Ruby", "Ruby on Rails", "Angular", "JQuery", "JSON", "CoffeeScript", "Digital Ocean", "Jasmine", "Git", "Backbone", "Three.js", "CSS", "Bootstrap", "Sinatra", "AJAX", "PostgreSQL", "SQL", "RSPEC", "Bash", "C++", "C", "C#", "Python", "ImageMagik", "Materialize", "MATLAB", "WebGL", "Pascal", "Perl", "UNITY", "Visual Basic", "Redis", "Varnish", "Puffing Billing", "Heroku", "JCrop", "OAuth", "Selenium", "Poltergeist", "Capybara" ]
+
+skills.each do |skill|
+  Skill.create( name: skill )
+end
