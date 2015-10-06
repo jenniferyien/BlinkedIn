cd436aa6cbab1fc71b099e551ba46d70215ff26b
Esther Zinn <Bionicfemme@me.com>
2015-10-06 15:28:20 -0400

    Configuring imagemagick and carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :endorsements
+  mount_uploader :avatar, AvatarUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005223511) do
+ActiveRecord::Schema.define(version: 20151006185602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,9 +96,11 @@ ActiveRecord::Schema.define(version: 20151005223511) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "auth_hash"
+    t.text     "auth_hash"
+    t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
   add_foreign_key "alumnis", "locations"
