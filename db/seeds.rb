581a1878e20b6cd51ace5bb501423d85e295bda1
Andres Carter <carter.marino@gmail.com>
2015-10-07 10:59:57 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,19 @@ locations = [
 	]
 
 locations.each do |city, state|
-	Location.create( city: city, state: state )
+	Location.create!( city: city, state: state )
 end
 
-skills = [ "Javascript", "React.js", "Ruby", "Ruby on Rails", "Angular", "JQuery", "JSON", "CoffeeScript", "Digital Ocean", "Jasmine", "Git", "Backbone", "Three.js", "CSS", "Bootstrap", "Sinatra", "AJAX", "PostgreSQL", "SQL", "RSPEC", "Bash", "C++", "C", "C#", "Python", "ImageMagik", "Materialize", "MATLAB", "WebGL", "Pascal", "Perl", "UNITY", "Visual Basic", "Redis", "Varnish", "Puffing Billing", "Heroku", "JCrop", "OAuth", "Selenium", "Poltergeist", "Capybara" ]
+skills = [
+	"Javascript", "React.js", "Ruby", "Ruby on Rails", "Angular", "JQuery",
+	"JSON", "CoffeeScript", "Digital Ocean", "Jasmine", "Git", "Backbone",
+	"Three.js", "CSS", "Bootstrap", "Sinatra", "AJAX", "PostgreSQL", "SQL",
+	"RSPEC", "Bash", "C++", "C", "C#", "Python", "ImageMagik", "Materialize",
+	"MATLAB", "WebGL", "Pascal", "Perl", "UNITY", "Visual Basic", "Redis",
+	"Varnish", "Puffing Billing", "Heroku", "JCrop", "OAuth", "Selenium",
+	"Poltergeist", "Capybara"
+]
 
 skills.each do |skill|
-  Skill.create( name: skill )
+  Skill.create!( name: skill )
 end
