da26f61622ad87ba38ad3a00c6ea48d791c48e40
Jennifer Yien <jenniferllwu@gmail.com>
2015-10-08 18:28:01 -0400

    user status fix

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -1,23 +1,35 @@
 var Search = React.createClass({
 	render: function(){
-		console.log('alumni', this.props.alumnis);
-
 		var people = this.props.alumnis.map(function(alumni){
-
 				return(
-					<ul>
-					<li>{alumni.name}</li>
-					<li>{alumni.about}</li>
-					<li>{alumni.fun_fact}</li>
-					</ul>
+					<div className="thumbnail">
+						<img src={alumni.avatar.avatar.profile.url} />
+						<div className="caption">
+							<h3>{alumni.first_name} {alumni.last_name}</h3>
+
+					<li>{alumni.city}</li>
+					<li>{alumni.state}</li>
+						</div>
+					</div>
+				)
+		});
+		var company = this.props.employers.map(function(employer){
+				return(
+					<div className="thumbnail">
+					<li><img src={employer.avatar.avatar.profile.url} /></li>
+					<li>{employer.company_name}</li>
+					<li>{employer.city}</li>
+					<li>{employer.state}</li>
+					</div>
 				)
 		});
 
-
 		return(
-					<div>
+					<div className='row'>
+						<div className="col-sm-6 col-md-4">
 					{people}
-
+					{company}
+						</div>
 					</div>
 		)
 
