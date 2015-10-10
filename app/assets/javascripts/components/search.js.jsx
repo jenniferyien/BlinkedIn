071f6250c08a71a12328e4776134f23e589b5354
Jennifer Yien <jenniferllwu@gmail.com>
2015-10-10 15:51:25 -0400

    fog.rb readded

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -7,17 +7,37 @@ var Search = React.createClass({
 	componentDidMount: function(){
 		this.setState({alumnis: this.props.alumnis, employers: this.props.employers})
 	},
+	handleAlumni: function(event){
+		event.preventDefault;
+		this.setState({employers: []});
+	},
+	handleEmployer: function(event){
+		event.preventDefault;
+		this.setState({alumnis: []});
+	},
+	handleViewCount: function(event){
+		event.preventDefault;
+		var mostviews = this.props.alumnis.sort(function(a, b){
+			return (b.view - a.view)
+		});
+		this.setState({alumnis: mostviews});
+	},
+	handleClick: function(event){
+		event.preventDefault();
+		var unsort = React.findDOMNode(this.refs.unsort).text;
+		console.log(unsort);
+		this.setState({alumnis: this.props.alumnis, employers: this.props.employers});
+	},
 	handleChange: function(event){
+		event.preventDefault;
 		var sort = React.findDOMNode(this.refs.sort).value;
-		console.log(sort);
 		var sortedA = this.props.alumnis.filter(function(value){
 			return ( value.location_city == sort);
 		});
-		console.log(sortedA)
 		var sortedE = this.props.employers.filter(function(value){
 			return ( value.location_city == sort);
 		});
-		this.setState({alumnis: sortedA, employers: sortedE})
+		this.setState({alumnis: sortedA, employers: sortedE});
 	},
 
 	render: function(){
@@ -73,14 +93,30 @@ var Search = React.createClass({
 			)
 		})
 		return(
-			<div>
-				<div>
-						<select name="locationpick" form="locationselect" ref='sort' onChange={this.handleChange}>
-							<option value=""></option>
-							{locations}
-						</select>
+			<div className="row">
+				<div className='panel panel-default'>
+					<div className='panel-heading'>
+							<a href='#' className='btn btn-link' ref='unsort' onClick={this.handleClick}>Unsort</a>
+							<a href='#' className='btn btn-link' ref='alumnionly' onClick={this.handleAlumni}>Alumni Only</a>
+							<a href='#' className='btn btn-link' ref='employeronly' onClick={this.handleEmployer}>Employers Only</a>
+							<a href='#' className='btn btn-link' ref='view' onClick={this.handleViewCount}>Viewcount</a>
+						<div className="col-xs-3">
+							<select className="form-control" name="alumnisort" form="locationselect">
+								<option value="">Sort by Alumni</option>
+								<option value='' onChange={this.handleAlumni}>Alumni Only</option>
 
+							</select>
+						</div>
+						<div className="col-xs-3">
+							<select className="form-control" name="locationpick" form="locationselect" ref='sort' onChange={this.handleChange}>
+								<option value="">Sort by Location</option>
+								{locations}
+							</select>
+						</div>
+
+					</div>
 				</div>
+
 				<div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 							{people}
 							{company}
