330b511c0ea488b7c64b7b90811c7787eb0ea947
Jennifer Yien <jenniferllwu@gmail.com>
2015-10-10 17:55:11 -0400

    sort by view count

--- app/assets/javascripts/components/search.js.jsx
+++ app/assets/javascripts/components/search.js.jsx
@@ -1,41 +1,54 @@
 var Search = React.createClass({
+	// Initial values
 	getInitialState: function(){
 		return {alumnis: [],
 						employers: []
 			};
 	},
+	//Page will display values of alumni and employers based on search
 	componentDidMount: function(){
 		this.setState({alumnis: this.props.alumnis, employers: this.props.employers})
 	},
+	//if only alumni search set employer will be 0
 	handleAlumni: function(event){
 		event.preventDefault;
-		this.setState({employers: []});
+		this.setState({alumnis: this.props.alumnis, employers: []});
 	},
+	//if only employer search set alumni wiill be 0
 	handleEmployer: function(event){
 		event.preventDefault;
-		this.setState({alumnis: []});
+		this.setState({alumnis: [], employers: this.props.employers});
 	},
-	handleViewCount: function(event){
+	//sorting viewcount for most to least views for alumni
+	handleViewMCount: function(event){
 		event.preventDefault;
 		var mostviews = this.props.alumnis.sort(function(a, b){
 			return (b.view - a.view)
 		});
 		this.setState({alumnis: mostviews});
 	},
+	//sorting viewcount for least to most views for alumni
+	handleViewLCount: function(event){
+		event.preventDefault;
+		var leastviews = this.props.alumnis.sort(function(a,b){
+			return (a.view - b.view)
+		});
+		this.setState({alumnis: leastviews});
+	},
+	//if clicking on unsort, everything reverts back to search
 	handleClick: function(event){
 		event.preventDefault();
-		var unsort = React.findDOMNode(this.refs.unsort).text;
-		console.log(unsort);
 		this.setState({alumnis: this.props.alumnis, employers: this.props.employers});
 	},
+	//filtering search based on location
 	handleChange: function(event){
 		event.preventDefault;
 		var sort = React.findDOMNode(this.refs.sort).value;
 		var sortedA = this.props.alumnis.filter(function(value){
-			return ( value.location_city == sort);
+			return ( value.location_id == sort);
 		});
 		var sortedE = this.props.employers.filter(function(value){
-			return ( value.location_city == sort);
+			return ( value.location_id == sort);
 		});
 		this.setState({alumnis: sortedA, employers: sortedE});
 	},
@@ -48,6 +61,7 @@ var Search = React.createClass({
 						<h4 className="panel-title">
 							<a role="button" data-toggle="collapse" data-parent="#accordion" href={"#collapseOne"+alumni.user_id}>
 							{alumni.first_name} {alumni.last_name} | <small>{alumni.location_city}, {alumni.location_state}</small></a>
+							<span className="badge pull-right">Views: {alumni.view}</span>
 						</h4>
 					</div>
 					<div id={"collapseOne"+alumni.user_id} className="panel-collapse collapse in" role="tabpanel">
@@ -89,31 +103,30 @@ var Search = React.createClass({
 		});
 		var locations = this.props.locations.map(function(location){
 			return (
-				<option value={location.city}> {location.city}, {location.state}</option>
+				<option value={location.id}> {location.city}, {location.state}</option>
 			)
 		})
 		return(
 			<div className="row">
 				<div className='panel panel-default'>
 					<div className='panel-heading'>
-							<a href='#' className='btn btn-link' ref='unsort' onClick={this.handleClick}>Unsort</a>
-							<a href='#' className='btn btn-link' ref='alumnionly' onClick={this.handleAlumni}>Alumni Only</a>
-							<a href='#' className='btn btn-link' ref='employeronly' onClick={this.handleEmployer}>Employers Only</a>
-							<a href='#' className='btn btn-link' ref='view' onClick={this.handleViewCount}>Viewcount</a>
-						<div className="col-xs-3">
-							<select className="form-control" name="alumnisort" form="locationselect">
-								<option value="">Sort by Alumni</option>
-								<option value='' onChange={this.handleAlumni}>Alumni Only</option>
+							<div className="btn-group btn-group-sm" role="group">
+								<div class="form-group">
+										<a href='#' className='btn btn-xs btn-primary' ref='employeronly' onClick={this.handleEmployer}>Employers Only</a> &#160;
+										<a href='#' className='btn btn-xs btn-primary' ref='alumnionly' onClick={this.handleAlumni}>Alumni Only</a>
+										<label>&#160; Alumni View Count Sort &#160;</label>
+										<button type="button" onClick={this.handleViewMCount} className="btn btn-primary btn-xs btn-default"><span className="glyphicon glyphicon-arrow-up span12"></span></button>
+										<button type="button" onClick={this.handleViewLCount} className="btn btn-primary btn-xs btn-default"><span className="glyphicon glyphicon-arrow-down span12"></span></button>
+								</div>
+							</div>
 
-							</select>
-						</div>
-						<div className="col-xs-3">
-							<select className="form-control" name="locationpick" form="locationselect" ref='sort' onChange={this.handleChange}>
-								<option value="">Sort by Location</option>
-								{locations}
-							</select>
-						</div>
-
+					 		<div className="col-xs-3">
+								<select className="form-control input-sm" name="locationpick" form="locationselect" ref='sort' onChange={this.handleChange}>
+									<option>Sort by Location</option>
+									{locations}
+								</select>
+							</div>
+							<a href='#' className='btn btn-sm badge pull-right' ref='unsort' onClick={this.handleClick}>Unsort</a>
 					</div>
 				</div>
 
