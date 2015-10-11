99daceb7a7ed7de3d55ae866b5d70d012656da30
Jennifer Yien <jenniferllwu@gmail.com>
2015-10-11 15:29:05 -0400

    testing skills

--- app/assets/javascripts/Charts.js
+++ app/assets/javascripts/Charts.js
@@ -1,24 +1,48 @@
 $(function(){
 
-var numberOfUsers = gon.watch('everything', usercount)
+    var numberOfUsers = gon.watch('everything', usercount)
 
-function usercount(numberOfUsers) {
-  var alumni = [
-    {
-        value: numberOfUsers[0],
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Registered Alumni"
-    },
-    {
-        value: numberOfUsers[1],
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Registered Employers"
-    } ]
-      var myLine = new Chart(document.getElementById("myChart").getContext("2d")).Pie(alumni)
-      // var myBar = new Chart(document.getElementById("myBarChart").getContext("2d")).Bar(alumni)
-    };
+    function usercount(numberOfUsers) {
+      var alumni = [
+        {
+            value: numberOfUsers[0],
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Registered Alumni"
+        },
+        {
+            value: numberOfUsers[1],
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Registered Employers"
+        } ]
+          var myLine = new Chart(document.getElementById("myChart").getContext("2d")).Pie(alumni)
+        };
 
+      function skills(){
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,0.8)",
+                    highlightFill: "rgba(220,220,220,0.75)",
+                    highlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.5)",
+                    strokeColor: "rgba(151,187,205,0.8)",
+                    highlightFill: "rgba(151,187,205,0.75)",
+                    highlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+        // var myBar = new Chart(document.getElementById("myBarChart").getContext("2d")).Bar(alumni)
+
+      };
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def search
+      # Search database by alumni, employers
     if params[:search]
       @alumnis = Alumni.where('name iLIKE ? OR first_name iLIKE ? OR last_name iLIKE ? OR email iLIKE ? OR city iLIKE ? OR state iLIKE ? OR about iLIKE ? OR q1 iLIKE ? OR q2 iLIKE ? OR q3 iLIKE ? OR fun_fact iLIKE ? OR position iLIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").eager_load(:user, :location, :skills)
       @employers = Employer.where('city iLIKE ? OR state iLIKE ? OR company_name iLIKE ? OR company_type iLIKE ? OR website iLIKE ? OR description iLIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").eager_load(:location)
@@ -20,6 +21,7 @@ class UsersController < ApplicationController
     @alumnis = Alumni.all
     @employers = Employer.all
 
+    # info for piechart on front page
     @alumninum = Alumni.all.count
     gon.watch.alumninum = @alumninum
     @employernum = Employer.all.count
@@ -29,6 +31,10 @@ class UsersController < ApplicationController
       @alumninum, @employernum
     ]
     gon.watch.everything = @everything
+
+    @javascript = Endorsement.where(skill_id: 1).count
+
+
   end
 
   # GET /users/1
