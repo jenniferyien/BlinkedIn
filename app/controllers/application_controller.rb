89d597e6f20078669b367041362a142fea88c190
Jennifer Yien <jenniferllwu@gmail.com>
2015-10-12 12:01:41 -0400

    controller sessions edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:uid]) if session[:uid]
+    if session[:uid]
+    @current_user ||= User.find(session[:uid])
+    end
   end
   helper_method :current_user
 end
